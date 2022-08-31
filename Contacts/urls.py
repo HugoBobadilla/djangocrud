5246c4a6ab0bcc8e166e74a665fd5ad60338a177
HugoBobadilla <hugoabobadilla1998@gmail.com>
2022-08-30 23:52:06 -0400

    Fix styles, routes and templates

--- Contacts/urls.py
+++ Contacts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('/', views.retrieve_contact, name='retrieve-contact'),
+  path('', views.retrieve_contact, name='retrieve-contact'),
   path('create/', views.crate_contact, name='create-contact'),
   path('update/<int:pk>', views.update_contact, name='update-contact'),
   path('delete/<int:pk>', views.delete_contact, name='delete-contact')
